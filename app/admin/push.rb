3f441f75cf7880fc611c0d6d97705fde3efeaedb
Vishal Vijay <0vishalvijay0@gmail.com>
2014-08-12 00:22:46 +0530

    added option in push type

--- app/admin/push.rb
+++ app/admin/push.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Push do
   permit_params :message, :ptype
+
+  form do |f|
+    f.inputs do
+      f.input :message
+      f.input :ptype, collection: [['Buy', 'buy'], ['Sell', 'sell']]
+    end
+    f.actions
+  end
 end
